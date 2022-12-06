c26903270715cd8547e178eb0f9f45c437158943
mershad-manesh <105240903+mershad-manesh@users.noreply.github.com>
2022-12-06 10:43:33 -0500

    Update web_connector.py

--- Jira-tools/library/web_connector.py
+++ Jira-tools/library/web_connector.py
@@ -4,11 +4,11 @@ class web_connector:
     def __init__(self) -> None:
         self._session=requests.Session()
     
-    def get(self,url,header="",param="") -> None:
-        return self._session.get(url,headers=header,params=param)
+    def get(self,url,header="",param="",auth="") -> None:
+        return self._session.get(url,headers=header,params=param,auth=auth)
     
-    def post(self,url,data,header="") -> None:
-        return self._session.post(url,data,headers=header)
+    def post(self,url,data,header="",param="") -> None:
+        return self._session.post(url,data,headers=header,params=param)
 
     def printCookies(self):
         print(self._session.cookies.get_dict())
