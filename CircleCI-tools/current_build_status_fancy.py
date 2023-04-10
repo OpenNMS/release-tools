955433cbf4de2c3cbebd352b31ea643d146574a5
mershad-manesh <105240903+mershad-manesh@users.noreply.github.com>
2023-04-10 15:49:28 -0400

    Update current_build_status_fancy.py

--- CircleCI-tools/current_build_status_fancy.py
+++ CircleCI-tools/current_build_status_fancy.py
@@ -19,20 +19,27 @@ def getPipelinesInformation(maxitems=1,branch="develop",prefix="",project_slug="
 
     last_status=""
     prefix+=" "
+    overall_status=""
     for a in _pipelines['items'][0:1]:
         _pipelines_workflows=circleci_handler.retrievePipelineWorkflow(str(a['id']))
+         
         for b in _pipelines_workflows['items']:
             if b['status'] in ["failed","failing"]:
                 last_status="failed"
             elif b['status'] in ["running"]:
                 if last_status not in ["failed"]:
                     last_status="running"
+                _jobs=circleci_handler.retrieveWorkflowJobs(b['id'])
+                for j in _jobs['items']:
+                    if j['status'] not in ["running",'blocked'] and overall_status not in ["failed","failing"]:
+                        overall_status=j['status']
             elif b['status'] in ["success"]:
                 if last_status not in ["failed","running","cancelled"]:
                     last_status="success"
             else:
-                last_status=b['status']      
-    return last_status
+                last_status=b['status'] 
+            
+    return [last_status,overall_status]
 
 def getData(releases_info):
     data={}
@@ -53,7 +60,7 @@ def generate_table(data) -> Table:
     table.add_column("Status")
 
     for row in data:
-        value = data[row]
+        value = data[row][0]
         if value in 'success':
             _color="[green3]"
             _msg="Successful"
@@ -62,7 +69,11 @@ def generate_table(data) -> Table:
             _msg="Failed"
         elif value in 'running':
             _color="[turquoise2]"
-            _msg="Running"
+            if data[row][1]:
+                _msg="Running ("+data[row][1]+")"
+            else:
+                _msg="Running"
+
         else:
             _color="[bright_magenta]"
             _msg=value
@@ -91,3 +102,5 @@ if __name__ == "__main__":
         live.update(generate_table(_data))
         time.sleep(1800)
         _data=getData(releases_info)
+
+
