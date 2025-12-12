18f7701757dccf1832998185af848944ea78333a
mershad-manesh <105240903+mershad-manesh@users.noreply.github.com>
2025-12-12 09:53:09 -0500

    Update jira.py to account for limit of 100

--- Release-scripts/day1/tools/jira/jira.py
+++ Release-scripts/day1/tools/jira/jira.py
@@ -48,10 +48,11 @@ class JIRA:
 
         self.LOGIN_CREDENTIAL=HTTPBasicAuth(config_handler.get("jira","Emailaddress"),config_handler.get("jira","Authtoken"))
 
+
     def getFixedIssues(self,release_name):
         payload = {
             "jql": self.CONFIGURATIONS["QUERIES"]["fixed_issues"].replace("#release_info#",release_name),
-            "maxResults": 1000,
+            "maxResults": 100,
             "fields": ["key", "summary", "issuetype", "status", "assignee"]
         }
         _output=self.HTTP_CONNECTOR.post(
@@ -61,10 +62,33 @@ class JIRA:
             auth=self.LOGIN_CREDENTIAL
         )
 
+        output_data=_output.json()
+
+        if "nextPageToken" in output_data:
+            while "nextPageToken" in output_data:
+                payload["nextPageToken"] = output_data["nextPageToken"]
+                _next_page=self.HTTP_CONNECTOR.post(
+                self.CONFIGURATIONS["SERVER_URL"] + self.CONFIGURATIONS["APIs"]["search"],
+                    data=json.dumps(payload),
+                    header={"Content-Type":"application/json","Accept":"application/json"},
+                    auth=self.LOGIN_CREDENTIAL
+                )
+                if _next_page.status_code == 200:
+                    next_page_data=_next_page.json()
+                    output_data["issues"].extend(next_page_data["issues"])
+                    if "nextPageToken" in next_page_data:
+                        output_data["nextPageToken"]=next_page_data["nextPageToken"]
+                    else:
+                        del output_data["nextPageToken"]
+                else:
+                    print("SOMETHING WENT WRONG")
+                    break
+
         if _output.status_code == 200:
-            return _output.json()
+            return output_data
         else:
             print("SOMETHING WENT WRONG")
+
     
     def getReleases(self):
         params={
